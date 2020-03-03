fd0699f881caf23f69d4f7c064f2eeec09b77c4b
Wouter de Bruin <wouter.de.bruin@capgemini.com>
2020-03-03 10:59:02 +0100

    added module

--- angular1/helloworld/src/app/app.module.ts
+++ angular1/helloworld/src/app/app.module.ts
@@ -1,7 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { StarWarsModule } from './star-wars/star-wars.module';
+// Alt+Shift+O for sorting imports
+// Alt+Shift+F for formatting.
+// Ctrl + Z for undo.
 
 @NgModule({
   declarations: [
@@ -9,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    StarWarsModule
   ],
   providers: [
 
